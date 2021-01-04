e2b3547fa5abec8812012b1874ffb2b6a064437d
Manos Kampourakis <ekampourakis@ceid.upatras.gr>
2021-01-04 11:47:55 +0200

    Titling change

--- photos.php
+++ photos.php
@@ -99,7 +99,7 @@
 			<div class="container">
 
 				<div class="d-flex justify-content-between align-items-center">
-					<h2>Φωτογραφίες</h2>
+					<h2>Μερικοί μαθητές μας</h2>
 					<ol>
 						<li><a href="index.html">Home</a></li>
 						<li>Φωτογραφίες</li>
